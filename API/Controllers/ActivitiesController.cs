447fde9e04093022589ada227727978842edf6ac
Josue F. Contreras <josue.contreras2@gmail.com>
2022-02-04 13:41:36 -0400

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,47 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ActivitiesController>
+        [HttpGet]
+
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+
+        // GET api/<ActivitiesController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
+        }
+
+        // POST api/<ActivitiesController>
+        //[HttpPost]
+
+
+        // PUT api/<ActivitiesController>/5
+        //[HttpPut("{id}")]
+
+
+        // DELETE api/<ActivitiesController>/5
+        //[HttpDelete("{id}")]
+
+    }
+}
