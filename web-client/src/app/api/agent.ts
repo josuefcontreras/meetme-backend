69fa1e0cb0c07fd6daf836c70e7f4f623f885931
Josue F. Contreras <josue.contreras2@gmail.com>
2022-02-15 23:36:47 -0400

    end of section 11

--- web-client/src/app/api/agent.ts
+++ web-client/src/app/api/agent.ts
@@ -21,6 +21,9 @@ axios.interceptors.response.use(
     const { data, status, config } = error.response!;
     switch (status) {
       case 400:
+        if (typeof data === "string") {
+          toast.error(data);
+        }
         if (config.method === "get" && data.errors.hasOwnProperty("id")) {
           history.push("/not-found");
         }
@@ -32,8 +35,6 @@ axios.interceptors.response.use(
             }
           }
           throw modalStateErrors.flat();
-        } else {
-          toast.error(data);
         }
         break;
       case 401:
@@ -55,8 +56,7 @@ const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-  post: <T>(url: string, body: {}) =>
-    axios.post<T>(url, body).then(responseBody),
+  post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
   del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
@@ -65,8 +65,7 @@ const Activities = {
   list: () => requests.get<Activity[]>("/activities"),
   details: (id: string) => requests.get<Activity>(`/activities/${id}`),
   create: (activity: Activity) => axios.post<void>("/activities", activity),
-  update: (activity: Activity) =>
-    axios.put<void>(`/activities/${activity.id}`, activity),
+  update: (activity: Activity) => axios.put<void>(`/activities/${activity.id}`, activity),
   delete: (id: string) => axios.delete<void>(`/activities/${id}`),
 };
 
--- web-client/src/features/errors/TestError.tsx
+++ web-client/src/features/errors/TestError.tsx
@@ -8,31 +8,23 @@ export default function TestErrors() {
   const [errors, setErrors] = useState(null);
 
   function handleNotFound() {
-    axios
-      .get(baseUrl + "buggy/not-found")
-      .catch((err) => console.log(err.response));
+    axios.get(baseUrl + "buggy/not-found").catch((err) => console.log(err.response));
   }
 
   function handleBadRequest() {
-    axios
-      .get(baseUrl + "buggy/bad-request")
-      .catch((err) => console.log(err.response));
+    axios.get(baseUrl + "buggy/bad-request").catch((err) => console.log(err.response));
   }
 
   function handleServerError() {
-    axios
-      .get(baseUrl + "buggy/server-error")
-      .catch((err) => console.log(err.response));
+    axios.get(baseUrl + "buggy/server-error").catch((err) => console.log(err.response));
   }
 
   function handleUnauthorised() {
-    axios
-      .get(baseUrl + "buggy/unauthorised")
-      .catch((err) => console.log(err.response));
+    axios.get(baseUrl + "buggy/unauthorised").catch((err) => console.log(err.response));
   }
 
   function handleBadGuid() {
-    axios.get(baseUrl + "activities/notaguid").catch((err) => console.log(err));
+    axios.get(baseUrl + "activities/notaguid").catch((err) => console.log(err.response));
   }
 
   function handleValidationError() {
@@ -45,30 +37,10 @@ export default function TestErrors() {
       <Segment>
         <Button.Group widths="7">
           <Button onClick={handleNotFound} content="Not Found" basic primary />
-          <Button
-            onClick={handleBadRequest}
-            content="Bad Request"
-            basic
-            primary
-          />
-          <Button
-            onClick={handleValidationError}
-            content="Validation Error"
-            basic
-            primary
-          />
-          <Button
-            onClick={handleServerError}
-            content="Server Error"
-            basic
-            primary
-          />
-          <Button
-            onClick={handleUnauthorised}
-            content="Unauthorised"
-            basic
-            primary
-          />
+          <Button onClick={handleBadRequest} content="Bad Request" basic primary />
+          <Button onClick={handleValidationError} content="Validation Error" basic primary />
+          <Button onClick={handleServerError} content="Server Error" basic primary />
+          <Button onClick={handleUnauthorised} content="Unauthorised" basic primary />
           <Button onClick={handleBadGuid} content="Bad Guid" basic primary />
         </Button.Group>
       </Segment>
