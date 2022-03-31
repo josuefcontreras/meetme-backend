dec29185d962efe4b15434afe464c8bbb26f839e
Josue F. Contreras <josue.contreras2@gmail.com>
2022-03-31 16:33:53 -0400

    Add Strict-Transport-Security headers

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -48,6 +48,14 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
+            else
+            {
+                app.Use(async (context, next) =>
+                {
+                    context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                    await next.Invoke();
+                });
+            }
 
             app.UseHttpsRedirection();
 
